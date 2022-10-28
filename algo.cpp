866598455331a93af1d249a6e85ad6f147855cb9
harshvse <harshvse@gmail.com>
2022-10-29 01:15:11 +0530

    added input sytem

--- algo.cpp
+++ algo.cpp
@@ -1,8 +1,19 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long int
+#define vi vector<int>
+#define vll vector<long long int>
+
 int main() {
-    int a = 0;
-    cout << "Hello World" << endl;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int i;
+    cin >> i;
+    cout << i;
+
     return 0;
 }
